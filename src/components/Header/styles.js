adf1baa3c84aeee1db0dfb1037bf94d5aee571fe
KennyAlmeida <kenny_almeida_25@hotmail.com>
2023-03-04 23:42:18 -0300

    sistema de controle financeiro

---
+++ src/components/Header/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    height: 150px;
+    text-align: center;
+    background-color: #4040ff;
+`;
+
+export const Header = styled.h1`
+    padding-top: 25px
+`;
+
+export const Title = styled.div`
+    paddind-top: 20px;
+    color: #fff;
+
+`;
+
+
+
+// Path: src\components\header\styles.js
+
